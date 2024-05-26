3f02b8db9157b921fdd79b12841ce66f93297944
Michael Wonng <michael@wonng.com>
2024-05-26 22:58:16 +1000

    fix import and default

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 
+import { BatchService } from "./src/batchService.js";
+
 export { BatchService } from "./src/batchService.js";
 export { singleTaskHandler } from "./src/helpers/index.js";
-
 export default BatchService;
